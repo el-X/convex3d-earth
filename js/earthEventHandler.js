073a29c8d182eb2c2406166a0b5e999f0a21f10d
Vitmonk <tmb@live.de>
2015-02-09 15:59:28 +0100

    - Button zum Switchen der Texturauflösung hinzugefügt. Standardmäßig wird die Textur mit niedriger Auflösung geladen, damit schwache Rechner auch was zum sehen bekommen.

--- js/earthEventHandler.js
+++ js/earthEventHandler.js
@@ -2,6 +2,11 @@
 function Earth() {
 }
 ;
+// Static variables
+Earth.imgNormalLow = "img/blue_marble_low.jpg";
+Earth.imgBoundariesLow = "img/blue_marble_boundaries_low.jpg";
+Earth.imgNormalHigh = "img/blue_marble_high.jpg";
+Earth.imgBoundariesHigh = "img/blue_marble_boundaries_high.jpg";
 
 /**
  * Resets the view of the earth to the prime meridian (Greenwich) and the
@@ -21,17 +26,47 @@ Earth.resetView = function () {
  * @param button used for toggling its background color
  */
 Earth.toggleCountryBoundaries = function (button) {
-    var imgNormal = "img/blue_marble.png";
-    var imgBoundaries = "img/blue_marble_boundaries.png";
-    var earthTexture = document.getElementById("earthTexture");
 
-    if (button.style.backgroundColor !== "blue") {
-        earthTexture.setAttribute("url", imgBoundaries);
+    var earthTexture = document.getElementById("earthTexture");
+    var textureUrl = earthTexture.getAttribute("url");
+
+    if (textureUrl === Earth.imgNormalLow) {
+        earthTexture.setAttribute("url", Earth.imgBoundariesLow);
         button.style.backgroundColor = "blue";
-    } else {
-        earthTexture.setAttribute("url", imgNormal);
+    } else if (textureUrl === Earth.imgBoundariesLow) {
+        earthTexture.setAttribute("url", Earth.imgNormalLow);
+        button.style.backgroundColor = "transparent";
+    } else if (textureUrl === Earth.imgNormalHigh) {
+        earthTexture.setAttribute("url", Earth.imgBoundariesHigh);
+        button.style.backgroundColor = "blue";
+    } else if (textureUrl === Earth.imgBoundariesHigh) {
+        earthTexture.setAttribute("url", Earth.imgNormalHigh);
         button.style.backgroundColor = "transparent";
     }
 };
 
+/**
+ * Toggles the texture resolution of the earth whenever the appropriate button has
+ * been clicked.
+ *
+ * @param button used for toggling the texture resolution
+ */
+Earth.toggleResolution = function (button) {
+    var earthTexture = document.getElementById("earthTexture");
+    var textureUrl = earthTexture.getAttribute("url");
+
+    if (textureUrl === Earth.imgNormalLow) {
+        earthTexture.setAttribute("url", Earth.imgNormalHigh);
+        button.style.backgroundColor = "blue";
+    } else if (textureUrl === Earth.imgNormalHigh) {
+        earthTexture.setAttribute("url", Earth.imgNormalLow);
+        button.style.backgroundColor = "transparent";
+    } else if (textureUrl === Earth.imgBoundariesHigh) {
+        earthTexture.setAttribute("url", Earth.imgBoundariesLow);
+        button.style.backgroundColor = "transparent";
+    } else if (textureUrl === Earth.imgBoundariesLow) {
+        earthTexture.setAttribute("url", Earth.imgBoundariesHigh);
+        button.style.backgroundColor = "blue";
+    }
+};
 
